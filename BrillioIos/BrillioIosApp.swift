3a6eb65892270f39f1349657174f0249e787b11b
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-04-16 14:41:28 -0600

    first commit

---
+++ BrillioIos/BrillioIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrillioIosApp.swift
+//  BrillioIos
+//
+//  Created by daniel ortiz millan on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BrillioIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
